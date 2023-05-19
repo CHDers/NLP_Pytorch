0703a2d597a19acf6ca8d6999445389334626a3b
Yanjun_Hao <haoyanjun1996@163.com>
2023-05-19 17:50:31 +0800

    Add autograd

---
+++ autograd.py
@@ -0,0 +1,26 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from rich import print
+
+# loss = w * 2
+
+# 随机初始化
+w = torch.tensor([99.995], requires_grad=True)
+
+# 训练
+loop = []
+
+for i in range(1000):
+    # 前向传播
+    loss = torch.pow(w, 2)
+    loop.append(loss.detach().numpy())
+    print(f"eopch={i}, w={w}, loss={loss}")
+    # 反向传播
+    loss.backward()
+    # 梯度下降
+    w.requires_grad_(False).add_(-0.001*w.grad).requires_grad_(True)
+    w.grad.zero_()
+
+plt.plot(loss)
+plt.show()
